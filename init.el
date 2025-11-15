4995c809c7ffa17b94e7a7816d25bcc7845a764f
angel.dhakal <angeldhakal97@gmail.com>
2025-11-15 14:02:58 -0500

    refactored emacs config from my old one and mage some good changes.

---
+++ init.el
@@ -0,0 +1,42 @@
+;;; init.el --- entry point -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; Entry point for my Emacs config
+
+;;; Code:
+
+;; Add Lisp directories
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "lisp/lang" user-emacs-directory))
+
+;; Load modules in order
+(load "core")
+(load "mypackage")
+(load "evil-config")
+(load "leader")
+(load "ui")
+(load "editing")
+(load "completion")
+(load "org-config")
+(load "git")
+
+;; Languages
+(load "cc")
+(load "astro")
+
+
+;;; init.el ends here
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-show-quick-access t nil nil "Customized with use-package company")
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ lisp/vterm.el
@@ -0,0 +1,9 @@
+;;; vterm.el --- VTerm terminal configuration -*- lexical-binding: t; -*-
+;;; Commentary:
+;; vterm splits
+
+;;; Code:
+;; VTerm - Fast terminal emulator
+
+(provide 'vterm)
+;;; vterm.el ends here
