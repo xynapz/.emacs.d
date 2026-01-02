7496fe1008b64f4400708b5d7eefe0451a6da77f
xynapz <angeldhakal97@gmail.com>
2026-01-01 19:00:33 -0500

    refactor: remove evil mode and associated package configurations

--- lisp/xz-editor.el
+++ lisp/xz-editor.el
@@ -38,59 +38,6 @@
 ;; Whitespace
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-;; Evil Mode
-(use-package evil
-  :ensure t
-  :init
-  (setq evil-want-integration t
-        evil-want-C-u-scroll t
-        evil-want-C-d-scroll t
-        evil-want-C-u-delete t
-        evil-undo-system 'undo-redo
-        evil-split-window-below t
-        evil-vsplit-window-right t)
-  :config
-  (evil-mode 1)
-
-  ;; C-g to exit insert state
-  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-
-  ;; jk to escape
-  (define-key evil-insert-state-map (kbd "j")
-    (lambda () (interactive)
-      (let ((evt (read-event nil nil 0.15)))
-        (if (and evt (eq evt ?k))
-            (evil-normal-state)
-          (insert "j")
-          (when evt (push evt unread-command-events)))))))
-
-;; Folding (required for evil-fold-action)
-(add-hook 'prog-mode-hook 'hs-minor-mode)
-
-(use-package evil-collection
-  :after evil
-  :ensure t
-  :init
-  ;; Must be set BEFORE loading evil-collection
-  (setq evil-collection-setup-minibuffer t
-        evil-collection-calendar-want-org-bindings t)
-  :config
-  (evil-collection-init)
-  ;; Force dired init if it missed the boat
-  (with-eval-after-load 'dired
-    (evil-collection-init 'dired)))
-
-(use-package evil-surround
-  :after evil
-  :ensure t
-  :config
-  (global-evil-surround-mode 1))
-
-(use-package evil-commentary
-  :after evil
-  :ensure t
-  :config
-  (evil-commentary-mode 1))
 
 (provide 'xz-editor)
 ;;; xz-editor.el ends here
